23427abfa64f3e95e72fcea1aa53a4b45bc4325f
Steven Swanson <swanson@cs.ucsd.edu>
2023-08-24 05:26:10 +0000

    less interactive

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,7 @@
 # copied from here:  https://docs.docker.com/engine/install/ubuntu/
 
 apt-get update
-apt-get install ca-certificates curl gnupg
+apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,5 +12,5 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
      tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 docker run hello-world
