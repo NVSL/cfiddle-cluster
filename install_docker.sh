ee8528772190543f34fa9b5569062c5594e44326
Steven Swanson <swanson@cs.ucsd.edu>
2023-08-24 05:16:17 +0000

    install_docker.sh script

---
+++ install_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# copied from here:  https://docs.docker.com/engine/install/ubuntu/
+
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker run hello-world
