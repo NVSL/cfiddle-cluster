e65b18f7b0c08550ae410898c0888230c5fd988c
Steven Swanson <swanson@cs.ucsd.edu>
2023-09-07 19:08:40 +0000

    better unbuilding

--- unbuild_cluster.sh
+++ unbuild_cluster.sh
@@ -2,9 +2,9 @@
 
 # The goal of this script is to clean up enough so that `build_cluster.sh` will run correctly.
 # 
-# Note it does some general tidying as well, so if you are using this
-# machine for anything else, you should check to make sure it doesn't
-# do something you don't want.
+# Note it does some general tidying of docker as well, so if you are
+# using this machine for anything else, you should check to make sure
+# it doesn't do something you don't want.
 
 # First stop the cluster:
 
@@ -37,6 +37,8 @@ cp /etc/exports /etc/exports.bak
 grep -v cfiddle_cluster < /etc/exports.bak > /etc/exports
 exportfs -ra
 
+# Remove munge key
+rm -rf /etc/munge
 
 # Tear down swarm
 
